ffabdda2ebc89c59368690e86d271212c8688c81
Matzi24GR <ggeomat@gmail.com>
2023-01-18 02:34:14 +0200

    Add hashed user files

---
+++ data/adminsHashed.sql
@@ -0,0 +1,17 @@
+-- Adminer 4.8.1 MySQL 5.5.5-10.8.3-MariaDB-1:10.8.3+maria~jammy dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+SET NAMES utf8mb4;
+
+INSERT INTO `admins` (`id`, `username`, `password`) VALUES
+(1,	'tguillon0',	'$2y$10$1CV/W.S6uiw0BnzTnEPK4.rjNhPeDwQqbhaGey8sO7Cz6tMzNXFbC'),
+(2,	'ljanz1',	'$2y$10$UoyKApSZgV0DAyPYX9swmeeXE1OSVT1GfqMt2a7T46GZD3RLUB0zC'),
+(3,	'gmariolle2',	'$2y$10$B3hsLiska2rcT88jCp6B8O/NO3kLX1r4OX1Yd5IJoCqUSPoehOifO'),
+(4,	'gcolombier3',	'$2y$10$m0jKIRBneW.jgIwVF3iJJOV6urZI7VDVEDl10Uj3CuH0QGDBZf3qa'),
+(5,	'cdripp4',	'$2y$10$PSjeEEjCBm/gF4/GZMFwoOgbw23RZyKgfrS8A/.RoSdQmizoeUJuS');
+
+-- 2023-01-18 00:31:36
