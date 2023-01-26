e3243ea38d63f74151049d59b478f866e1b24f79
Matzi24GR <ggeomat@gmail.com>
2023-01-27 00:38:36 +0200

    Fix book now on myappointment page

--- php/patient/myAppointments.php
+++ php/patient/myAppointments.php
@@ -119,12 +119,12 @@ if($result->num_rows > 0){
                             }
                         }else{
                             echo "<b class='h2 text-danger'>You don't have any appointments with our doctors</b>";
-                            echo '<button href="../doctor/myProfile.php" type="button" class="btn btn-block btn-danger col-sm-6 mx-auto mt-4 mb-4">
+                            echo '<a href="../patient/searchDocs.php" class="btn btn-block btn-danger col-sm-6 mx-auto mt-4 mb-4">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar" viewBox="0 0 16 16">
                             <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"></path>
                             </svg>
                             Book Now!
-                            </button>';
+                            </a>';
                         }
                         ?>
                 </div>
