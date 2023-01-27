7170d24db8dbd06996f0b3b91ca8b7b7c0a0870c
Matzi24GR <ggeomat@gmail.com>
2023-01-27 05:28:12 +0200

    Add admin appointment creation and deletion

--- php/admin/manageAppointments.php
+++ php/admin/manageAppointments.php
@@ -2,11 +2,25 @@
 session_start();
 include("../connect.php");
 
-// Appointment Confirmation
 if (isset($_POST)) {
+    $operation = isset($_POST['operation']) ? $_POST['operation'] : null;
     $id = isset($_POST['id']) ? $_POST['id'] : null;
-    if ($id != null) {
+    // Appointment Confirmation
+    if ($id != null && $operation == "confirm") {
         $query ="UPDATE appointments SET confirmed = 1 WHERE id={$id}";
+        $conn->query($query);
+    }
+    // Appointment Deletion
+    if ($id != null && $operation == "delete") {
+        $query ="DELETE FROM appointments WHERE id={$id}";
+        $conn->query($query);
+    }
+
+    if ($operation == "insert") {
+        $date = $_POST['date'];
+        $patient = $_POST['patient'];
+        $doctor = $_POST['doctor'];
+        $query ="INSERT INTO appointments (date, patientID, doctorID) VALUES ('$date', '$patient', '$doctor')";
         $conn->query($query);
     }
 }
@@ -30,6 +44,9 @@ if($result->num_rows > 0){
     <!--== Main Style CSS ==-->
     <link href="../assets/css/style.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
 <body>
 <div>
@@ -81,6 +98,64 @@ if($result->num_rows > 0){
         <div class="container-fluid text-center">
             <div class="col-sm-12 col-md-10 col-lg-8 mx-auto mt-4">
 
+                <h3>Appointments</h3>
+                <button type="button" data-bs-toggle="modal" data-bs-target="#insertModal" class="btn btn-block btn-primary col-sm-6 mx-auto mt-4 mb-4">
+                    Insert Appointment
+                </button>
+
+                <!-- Modal -->
+                <div class="modal fade" id="insertModal" tabindex="-1" role="dialog" aria-labelledby="insertModal" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLongTitle">Insert Appointment</h5>
+                                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <form id="insertForm" method='post' action='manageAppointments.php'>
+                                    <input type='hidden' id='operation' name='operation' value=insert>
+                                    PATIENT
+                                    <select name="patient" class="form-select" size="10">
+                                        <?php
+                                        $query ="SELECT * FROM patients";
+                                        $result = $conn->query($query);
+                                        if($result->num_rows > 0){
+                                            $patients = mysqli_fetch_all($result, MYSQLI_ASSOC);
+                                        }
+                                        foreach ($patients as $value) {
+                                            echo "<option value='{$value['id']}'>{$value['name']}</option>";
+                                        }
+                                        ?>
+                                    </select>
+                                    Doctor
+                                    <select name="doctor" class="form-select" size="10">
+                                        <?php
+                                        $query ="SELECT * FROM doctors";
+                                        $result = $conn->query($query);
+                                        if($result->num_rows > 0){
+                                            $patients = mysqli_fetch_all($result, MYSQLI_ASSOC);
+                                        }
+                                        foreach ($patients as $value) {
+                                            echo "<option value='{$value['id']}'>{$value['name']}, {$value['specialisation']}, {$value['location']}</option>";
+                                        }
+                                        ?>
+                                    </select>
+                                    <br>
+                                    Date
+                                    <input type="datetime-local" name="date">
+                                </form>
+
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <input type="submit" form="insertForm" class="btn btn-primary" value="Insert">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 <div class="table-responsive  table-sm mb-5">
                     <table class="table table-hover table-bordered">
                         <tr>
@@ -91,7 +166,8 @@ if($result->num_rows > 0){
                         </tr>
                         <?php
                         foreach ($appointments as $value) {
-                            $form = "<form method='post'' action='myAppointments.php'>
+                            $form = "<form method='post' action='manageAppointments.php'>
+                                        <input type='hidden' id='operation' name='operation' value=confirm>
                                         <input type='hidden' id='id' name='id' value={$value['id']}>
                                         <input type='submit' class='btn btn-outline-primary' name='' value='Confirm'>
                                      </form>";
@@ -101,7 +177,19 @@ if($result->num_rows > 0){
                             <td>{$value['pName']}</td>
                             <td>{$value['dName']}</td>
                             <td>{$value['date']}</td>
-                            <td>{$confirmation}</td>
+                            <td>
+                                $confirmation
+                                <form method='post' action='manageAppointments.php'>
+                                  <input type='hidden' id='operation' name='operation' value=edit>
+                                  <input type='hidden' name='id' value={$value['id']}>
+                                  <input type='submit' value='Change Date' class='btn btn-warning'>
+                                </form>
+                                <form method='post' action='manageAppointments.php'>
+                                  <input type='hidden' id='operation' name='operation' value=delete>
+                                  <input type='hidden' name='id' value={$value['id']}>
+                                  <input type='submit' value='Delete' class='btn btn-danger'>
+                                </form>
+                            </td>
                             </tr>
                         ";
                         }
